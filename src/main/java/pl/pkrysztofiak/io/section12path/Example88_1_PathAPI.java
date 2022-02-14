cf5c9f99a086ff31056a7905aad2cb30b58b28f7
Przemek Krysztofiak <p.krysztofiak@protonmail.com>
2022-02-14 20:39:58 +0100

    Byte array streams #2

---
+++ src/main/java/pl/pkrysztofiak/io/section12path/Example88_1_PathAPI.java
@@ -0,0 +1,12 @@
+package pl.pkrysztofiak.io.section12path;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Example88_1_PathAPI {
+
+    public static void main(String[] args) {
+
+
+    }
+}
